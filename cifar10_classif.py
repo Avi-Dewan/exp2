89f38e22447f885d8e367e8c13deeb086895020f
ShinraTenseiRe <avidewan05@gmail.com>
2024-08-09 05:49:03 +0600

    done

--- cifar10_classif.py
+++ cifar10_classif.py
@@ -163,7 +163,7 @@ if __name__ == "__main__":
     parser.add_argument('--batch_size', default=128, type=int)
     parser.add_argument('--num_classes', default=5, type=int)
     parser.add_argument('--model_name', type=str, default='resnet18_cifar10_classif_5')
-    parser.add_argument('--dataset_root', type=str, default='data/datasets/CIFAR/')
+    parser.add_argument('--dataset_root', type=str, default='./data/datasets/CIFAR/')
     parser.add_argument('--exp_root', type=str, default='./data/experiments/')
     args = parser.parse_args()
     args.cuda = torch.cuda.is_available()
